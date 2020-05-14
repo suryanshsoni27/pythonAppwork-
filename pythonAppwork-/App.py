ad5a2be0c84264c1902f084cc8ec90025505ede5
suryansh soni <49648748+suryanshsoni27@users.noreply.github.com>
2020-05-14 01:13:36 -0500

    update

--- pythonAppwork-/App.py
+++ pythonAppwork-/App.py
@@ -1,3 +1,11 @@
-import Tkinter 
-top = Tkinter.Tk()
-top.mainloop()
+import random
+from random import choice
+import matplotlib.pyplot as plt 
+
+list = [1,2]
+x = random.choice(list)
+y = random.choice(list)
+print(x)
+print(y)
+plt.plot(x,y)
+plt.show()
