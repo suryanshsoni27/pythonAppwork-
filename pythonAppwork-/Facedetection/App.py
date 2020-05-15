607791f0bdb35452098595be9b1d26281d289a90
suryansh soni <49648748+suryanshsoni27@users.noreply.github.com>
2020-05-15 02:04:47 -0500

    face

--- pythonAppwork-/Facedetection/App.py
+++ pythonAppwork-/Facedetection/App.py
@@ -1,5 +1,10 @@
-import opencv
+import cv2
 
-img = cv2.imread("C:\\Users\\hsoni\\OneDrive\\Pictures\\Screenshots")
-
-print('img::',img)
+img = cv2.imread("C:\\Users\\hsoni\\Desktop\\goku.jpg")
+resizedimg = cv2.resize(img, (int(img.shape[1]/2),int(img.shape[0]/2)))
+print('img::',img.shape)
+print('img::',resizedimg)
+cv2.imshow("goku",img)
+cv2.imshow("resized goku",resizedimg)
+cv2.waitKey(2220)
+cv2.destroyAllWindows()
---
+++ pythonAppwork-/Facedetection/MainWork.py
@@ -0,0 +1,15 @@
+import cv2 
+face_cascade = cv2.CascadeClassifier("C:\\Users\\hsoni\\Desktop\\PyVSC\\pythonAppwork-\\Facedetection\\cascade.xml")
+img = cv2.imread("C:\\Users\\hsoni\\Desktop\\goku.jpg")
+
+gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.05, minNeighbors=5)
+
+for x,y,w,h in faces:
+    img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+
+resized = cv2.resize(img,(int(img.shape[1]),int(img.shape[0])))
+cv2.imshow("Gray",resized)
+
+cv2.waitKey(0)
